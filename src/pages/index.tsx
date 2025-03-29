95a7245e602c5c62363220a6d1636110f17d8eb6
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2025-03-29 13:39:28 +0100

    fix title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,8 +14,9 @@ export default function Home(): ReactNode {
 
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Self-Hosted Server Management Tool`}
+      description="Free, Open-Source, and Self-Hosted Server Management Tool"
+    >
       <main className='relative'>
         <div className="absolute z-[999] -top-80 left-[50%] translate-x-[-50%] size-[300px] rounded-full bg-white/60 blur-[100px] dark:bg-indigo-500"></div>
         <div className="container mx-auto py-10 space-y-20">
