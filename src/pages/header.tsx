b658dd2a3b97e8faac69b452895805868d85bda5
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2025-03-29 16:30:49 +0100

    fix extra space in the home page

--- src/pages/header.tsx
+++ src/pages/header.tsx
@@ -6,13 +6,13 @@ import { ReactNode } from "react";
 
 export default function Header(): ReactNode {
     return (
-        <header className='container relative flex flex-col items-center justify-center py-10'>
+        <header className='container relative flex flex-col items-center justify-center'>
             <h1 className='font-bold text-[60px] text-indigo-600 dark:text-white mb-2'>VitoDeploy</h1>
             <WordRotate
-                className='text-3xl font-semibold mb-2'
+                className='text-3xl font-semibold mb-0'
                 words={["Open-Source", "Free", "Self-Hosted"]}
             />
-            <p className='text-3xl'>Server Management Tool</p>
+            <p className='text-3xl mb-2'>Server Management Tool</p>
             <div className="relative overflow-hidden rounded-[9px] border border-border mt-10 shadow-lg bg-[url(/img/services-light.png)] dark:bg-[url(/img/services-dark.png)] bg-cover bg-center">
                 <img src="/img/services-light.png" alt="VitoDeploy" className="max-h-[500px] opacity-0" />
                 <BorderBeam size={200} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home(): ReactNode {
     >
       <main className='relative'>
         <div className="absolute z-[999] -top-80 left-[50%] translate-x-[-50%] size-[300px] rounded-full bg-white/60 blur-[100px] dark:bg-indigo-500"></div>
-        <div className="container mx-auto py-10 space-y-20">
+        <div className="container mx-auto py-10 space-y-10">
           <Header />
           <Features />
           <Testimonial />
