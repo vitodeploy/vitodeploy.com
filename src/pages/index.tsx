2957962577ab299bdea7a6f7612d3555809f3291
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2025-10-26 11:00:46 +0100

    update home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Features from './features';
 import Header from './header';
 import { Testimonial } from '../components/testimonial';
 import Contributors from '../components/contributors';
-import Pricing from './pricing';
+// import Pricing from './pricing';
 
 export default function Home() {
   return (
@@ -13,7 +13,7 @@ export default function Home() {
         <div className="container mx-auto space-y-10 py-10">
           <Header />
           <Features />
-          <Pricing />
+          {/* <Pricing /> */}
           <Contributors />
           <Testimonial />
         </div>
